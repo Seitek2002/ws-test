82b902124ddf7e651b9e069751fc4829a31be00a
Seitek2002 <seitek_2002@bk.ru>
2023-08-04 16:52:53 +0600

    qwertyu

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+const messageInput = document.querySelector(".message")
+const uidInput = document.querySelector(".uid")
+
 const ws = new WebSocket("ws://89.208.106.189/ws/socket-server/chat/1");
 
 document.querySelector("button").addEventListener("click", () => {
     ws.send(JSON.stringify({
-        message: document.querySelector("input").value,
+        message: messageInput.value,
     }))
 })
 
 ws.onmessage = async (e) => {
     const data = JSON.parse(e.data)
     document.querySelector("ul").innerHTML += `<li>${data.result.message}</li>`
-    document.querySelector("input").value = ""
+    messageInput.value = ""
 }
 
 
